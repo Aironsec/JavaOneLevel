21916fe13c6dcac2fe52575e41dbe51d96c1c8f1
Aironsec <61885959+Aironsec@users.noreply.github.com>
2020-04-11 05:09:56 +0300

    HomeWork8 implement

---
+++ src/lesson8/TikTakToeGameRunGUI.java
@@ -0,0 +1,11 @@
+package lesson8;
+
+import javax.swing.*;
+
+public class TikTakToeGameRunGUI {
+
+    public static void main(String[] args) {
+        JFrame window = new TikTakToeGameGUI();
+        window.setVisible(true);
+    }
+}
